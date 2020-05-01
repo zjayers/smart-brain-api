45ada1b038d9738753af29aded360b9b673faab8
Zach Ayers <ayers.dev@icloud.com>
2020-05-01 10:59:55 -0500

    Authorization Added

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+const auth = require('./controllers/authorization');
+
 
 const app = express();
 
@@ -21,11 +23,12 @@ app.use(cors())
 app.use(bodyParser.json());
 app.get('/test', (req, res)=> { res.send('Working') })
 app.get('/', (req, res)=> { res.send(knex.users) })
-app.post('/signin', signin.handleSignin(knex, bcrypt))
+app.post('/signin', signin.signInAuthentication(knex, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, knex, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, knex)})
-app.put('/image', (req, res) => { image.handleImage(req, res, knex)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.get('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileGet(req, res, knex)})
+app.post('/profile/:id', auth.requireAuth, (req, res) => {profile.handleProfileUpdate(req, res, knex)});
+app.put('/image', auth.requireAuth, (req, res) => { image.handleImage(req, res, knex)})
+app.post('/imageurl', auth.requireAuth, (req, res) => { image.handleApiCall(req, res)})
 
 app.listen(3000, ()=> {
   console.log('app is listening on port 3000');
